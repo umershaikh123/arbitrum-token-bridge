a43193d07bbb617fd8e2dcba93920aa7e2da7f43
umershaikh123 <umershaikh217@gmail.com>
2024-06-06 14:09:28 +0500

    graph env

--- packages/arb-token-bridge-ui/src/util/client.ts
+++ packages/arb-token-bridge-ui/src/util/client.ts
@@ -4,7 +4,7 @@ import { HttpLink, ApolloClient , InMemoryCache } from "@apollo/client"
 export const getClient = new ApolloClient({
     cache: new InMemoryCache(),
     link: new HttpLink({
-      uri: "https://api.studio.thegraph.com/query/58809/nexus/v0.0.1",
+      uri: process.env.NEXT_PUBLIC_NEXUS_GRAPH_API,
     }),
   });
 
